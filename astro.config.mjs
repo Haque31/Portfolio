359ac06a52363ad5a9d5e28dfac77200101887d6
hawk <haquee1111@gmail.com>
2026-07-03 20:18:15 +0600

    chore: standardize npm and configure Vercel deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,7 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://haque31.github.io',
-  base: '/Portfolio',
+  site: 'https://mridul-haque-portfolio.vercel.app',
   output: 'static',
   integrations: [sitemap()],
   build: {
